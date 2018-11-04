5445304d97a9618c2b51ad8456a00ee89acc3340
enPiT-P31 <d802@oshima-k.ac.jp>
2018-11-05 07:12:45 +0900

    sakuseityuu

--- app/src/main/java/com/example/enpit_p31/hapverkaigosya/Main2Activity.kt
+++ app/src/main/java/com/example/enpit_p31/hapverkaigosya/Main2Activity.kt
@@ -3,10 +3,10 @@ package com.example.enpit_p31.hapverkaigosya
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 
-class MainActivity : AppCompatActivity() {
+class Main2Activity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_main2)
     }
 }
